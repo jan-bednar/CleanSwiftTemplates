3a7697b51a414493ef7a840795599c61b5c0a121
Jan Bednar <bednarjanuh@gmail.com>
2020-04-27 13:17:27 +0200

    typo fix

--- Clean Swift/Scene.xctemplate/CertificationViewController/___FILEBASENAME___ViewController.swift
+++ Clean Swift/Scene.xctemplate/CertificationViewController/___FILEBASENAME___ViewController.swift
@@ -70,6 +70,7 @@ final class ___VARIABLE_sceneName___ViewController: CertificationViewController,
         default:
             break
         }
-        
+
         return super.tableView(tableView, cellForRowAt: indexPath)
+    }
 }
